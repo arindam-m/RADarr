60c84427413bbc7103b67d327c67a8fe6d08a755
Craulback <92492424+Craulback@users.noreply.github.com>
2022-08-20 01:01:03 -0600

    better folder check

--- __init__.py
+++ __init__.py
@@ -55,13 +55,19 @@ mt_offset_rot_nor = 0.0
 
 ci, co = True, False
 operator_doff = True
+right_path = False
 
 # check user preferences for custom script directory
 custom_script_dir = bpy.utils.script_path_pref()
 if not custom_script_dir:
     # use default user script directory if not set
     custom_script_dir = bpy.utils.script_path_user()
-    if not "RADarr" in os.listdir(custom_script_dir + "/addons/"):
+    if os.path.exists(custom_script_dir + "/addons/"):
+        for f in os.listdir(custom_script_dir + "/addons/"):
+            if "RADarr" in f:
+                right_path = True
+                break
+    if not right_path:
         # last resort check all possible script locations
         for path in bpy.utils.script_paths():
             if os.path.exists(path + "/addons/"):
